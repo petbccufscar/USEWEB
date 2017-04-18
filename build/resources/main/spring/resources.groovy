ca4feed121d135db4a785c7fc126af8e8e8f8109
Felipe Sampaio <sampaio.motion@gmail.com>
2017-04-17 23:40:36 -0300

    Exclusão de arquivos desnecessários.

--- build/resources/main/spring/resources.groovy
+++
@@ -1,8 +0,0 @@
-// Place your Spring DSL code here
-beans = {
-    //TODO: Verificar melhor forma para configura o local
-    localeResolver(org.springframework.web.servlet.i18n.SessionLocaleResolver) {
-        defaultLocale = new Locale("pt","BR")
-        java.util.Locale.setDefault(defaultLocale)
-    }
-}
